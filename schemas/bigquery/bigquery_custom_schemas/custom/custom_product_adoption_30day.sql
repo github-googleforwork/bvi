3b0473769ff69d9d0287e4538ea4f988bfcac289
Daniel Paranhos <padaniel@google.com>
2018-07-18 10:26:59 -0300

    Fixed custom_product_adoption_30day query for Business deployments. The values were being wrongly calculated.

--- schemas/bigquery/bigquery_custom_schemas/custom/custom_product_adoption_30day.sql
+++ schemas/bigquery/bigquery_custom_schemas/custom/custom_product_adoption_30day.sql
@@ -1,8 +1,8 @@
 -- CUSTOM custom_product_adoption_30day
--- Review: 26/01/2018
+-- Review: 18/07/2018
 
 SELECT
-  DATE(YOUR_TIMESTAMP_PARAMETER) AS date,
+  adoption.date AS date,
   IFNULL(users.ou, 'undefined') AS ou,
   IFNULL(users.custom_1, 'undefined') AS custom_1,
   IFNULL(users.custom_2, 'undefined') AS custom_2,
@@ -17,31 +17,35 @@ SELECT
   SUM(adoption.unknown) AS unknown,
   SUM(adoption.calendar) AS calendar,
   SUM(adoption.gplus) AS gplus
-FROM 
-(SELECT 
-  DATE(YOUR_TIMESTAMP_PARAMETER) AS date,
+FROM
+(SELECT
+  DATE(YOUR_TIMESTAMP_PARAMETER) as date,
   data_email,
-  SUM(EXACT_COUNT_DISTINCT(data_email)) AS active_users_total,
-  SUM( CASE WHEN application_name = 'drive' AND parameters_name = 'doc_type' AND product = 'document' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as document,
-  SUM( CASE WHEN application_name = 'drive' AND parameters_name = 'doc_type' AND product = 'drawing' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as drawing,
-  SUM( CASE WHEN application_name = 'drive' AND parameters_name = 'doc_type' AND product = 'folder' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as folder,
-  SUM( CASE WHEN application_name = 'drive' AND parameters_name = 'doc_type' AND product = 'form' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as form,
-  SUM( CASE WHEN application_name = 'drive' AND parameters_name = 'doc_type' AND product = 'presentation' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as presentation,
-  SUM( CASE WHEN application_name = 'drive' AND parameters_name = 'doc_type' AND product = 'spreadsheet' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as spreadsheet,
-  SUM( CASE WHEN application_name = 'drive' AND parameters_name = 'doc_type' AND product = 'unknown' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as unknown,
-  SUM( CASE WHEN application_name = 'calendar' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as calendar,
-  SUM( CASE WHEN application_name = 'gplus' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as gplus
+  SUM( CASE WHEN app = 'document' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as document,
+  SUM( CASE WHEN app = 'drawing' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as drawing,
+  SUM( CASE WHEN app = 'spreadsheet' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as spreadsheet,
+  SUM( CASE WHEN app = 'presentation' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as presentation,
+  SUM( CASE WHEN app = 'folder' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as folder,
+  SUM( CASE WHEN app = 'form' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as form,
+  SUM( CASE WHEN app = 'unknown' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as unknown,
+  SUM( CASE WHEN app = 'calendar' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as calendar,
+  SUM( CASE WHEN app = 'gplus' THEN (INTEGER(EXACT_COUNT_DISTINCT(data_email))) ELSE 0 END ) as gplus
 FROM (
   SELECT
     id.time,
     STRFTIME_UTC_USEC(id.time,"%Y-%m-%d") AS date,
     actor.email AS data_email,
-    events.type,
-    events.name,
-    events.parameters.value AS product,
-    id.applicationName AS application_name,
-    events.parameters.name AS parameters_name,
-    INTEGER(COUNT(*)) AS event
+    CASE
+      WHEN id.applicationName = 'drive' AND events.parameters.name = 'doc_type' AND events.parameters.value = 'document' THEN 'document'
+      WHEN id.applicationName = 'drive' AND events.parameters.name = 'doc_type' AND events.parameters.value = 'drawing' THEN 'drawing'
+      WHEN id.applicationName = 'drive' AND events.parameters.name = 'doc_type' AND events.parameters.value = 'spreadsheet' THEN 'spreadsheet'
+      WHEN id.applicationName = 'drive' AND events.parameters.name = 'doc_type' AND events.parameters.value = 'presentation' THEN 'presentation'
+      WHEN id.applicationName = 'drive' AND events.parameters.name = 'doc_type' AND events.parameters.value = 'folder' THEN 'folder'
+      WHEN id.applicationName = 'drive' AND events.parameters.name = 'doc_type' AND events.parameters.value = 'form' THEN 'form'
+      WHEN id.applicationName = 'drive' AND events.parameters.name = 'doc_type' AND events.parameters.value = 'unknown' THEN 'unknown'
+      WHEN id.applicationName = 'calendar' THEN 'calendar'
+      WHEN id.applicationName = 'gplus' THEN 'gplus'
+    END AS app
   FROM
     [YOUR_PROJECT_ID:raw_data.audit_log]
   WHERE
@@ -49,14 +53,13 @@ FROM (
     AND _PARTITIONTIME >= DATE_ADD(YOUR_TIMESTAMP_PARAMETER, -30, "DAY")
     AND _PARTITIONTIME < DATE_ADD(YOUR_TIMESTAMP_PARAMETER,1,"DAY")
     AND id.applicationName IN ('drive', 'calendar', 'gplus')
-    AND events.type IS NOT NULL
-  GROUP BY 1, 2, 3, 4, 5, 6, 7, 8) data
-GROUP BY 1, 2, product, application_name, parameters_name) adoption
+    AND events.type IS NOT NULL) data
+GROUP BY 1, 2, app) adoption
 LEFT JOIN (
-  SELECT users_ou_list.ou AS ou, 
-  users_ou_list.email AS email, 
-  custom.custom_1 AS custom_1, 
-  custom.custom_2 AS custom_2, 
+  SELECT users_ou_list.ou AS ou,
+  users_ou_list.email AS email,
+  custom.custom_1 AS custom_1,
+  custom.custom_2 AS custom_2,
   custom.custom_3 AS custom_3
   FROM
     [YOUR_PROJECT_ID:users.users_ou_list] users_ou_list
@@ -66,7 +69,8 @@ LEFT JOIN (
   WHERE
     TRUE
     AND users_ou_list._PARTITIONTIME >= DATE_ADD(YOUR_TIMESTAMP_PARAMETER, -30, "DAY")
-    AND users_ou_list._PARTITIONTIME < DATE_ADD(YOUR_TIMESTAMP_PARAMETER,1,"DAY") GROUP BY 1,2,3,4,5) users
+    AND users_ou_list._PARTITIONTIME < DATE_ADD(YOUR_TIMESTAMP_PARAMETER,1,"DAY")
+    GROUP BY 1, 2, 3, 4, 5) users
 ON
   users.email = adoption.data_email
 GROUP BY 1, 2, 3, 4, 5
